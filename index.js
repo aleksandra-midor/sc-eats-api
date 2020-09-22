3b68d4657131fd452f5e7bc1a3f0089e176a00ea
octane\ola <ola_midor@outlook.com>
2020-09-22 20:28:26 +0200

    post function added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
+const data = require("./data.json");
+const fs = require("fs");
 const app = express();
 const { connect } = require("./config/database.js");
 app.use(express.json());
+
+console.log('---- APP START')
 
 app.get("/", (req, res) =>
   res.json({
@@ -11,28 +14,56 @@ app.get("/", (req, res) =>
 );
 
 app.get("/api/v1/restaurants", (req, res) => {
+  console.log(data.restarants);
   return res.json(data);
 });
 
-const data = {
-  restaurants: [],
-};
 // get, delete, post, put
 app.post("/api/v1/restaurants", (req, res) => {
   // console.log(req.body); // body --> sending body
-  const { body } = req;
-  body._id = data.restaurants.length + 1;
+  const body = req.body;
+  body.id = data.restaurants.length + 1;
   data.restaurants.push(body);
+  fs.writeFileSync("./data-test.json", JSON.stringify(data, null, 2));
+  return res.json(data);
+});
+
+app.put("/api/v1/restaurants", (req, res) => {
+  // console.log(req.body); // body --> sending body
+  const body = req.body;
+
+  const index = data.restaurants.findIndex(
+    (aRestaurant) => aRestaurant.id === +body.id
+  );
+  console.log(index, data.restaurants.length);
+  if (index > -1) {
+    data.restaurants.splice(index, 1);
+    data.restaurants.push(body);
+  }
+
+  fs.writeFileSync("./data-test.json", JSON.stringify(data, null, 2));
   return res.json(data);
 });
 
 app.get("/api/v1/restaurants/:restaurantId", (req, res) => {
   // getting a parameter
-  const { restaurantId } = req.params;
+  const restaurantId = req.params.restaurantId;
   const restaurant = data.restaurants.find(
-    (aRestaurant) => aRestaurant._id === +restaurantId
+    (aRestaurant) => aRestaurant.id === +restaurantId
   );
   return res.json(restaurant);
 });
+
+app.delete("/api/v1/restaurants/:restaurantId", (req, res) => {
+  // getting a parameter
+  const restaurantId = req.params.restaurantId;
+  const restaurant = data.restaurants.filter(
+    (aRestaurant) => aRestaurant.id !== +restaurantId
+  );
+
+  fs.writeFileSync("./data-test.json", JSON.stringify(restaurant, null, 2));
+  return res.json(restaurant);
+});
+
 connect();
 app.listen(4000, () => console.log("Running"));

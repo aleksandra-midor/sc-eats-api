1aa02113fce6d8c6d19e2ae55ef98fa857695dbf
octane\ola <ola_midor@outlook.com>
2020-09-24 22:21:11 +0200

    JWT authorization work in progress

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
 const express = require("express");
 const data = require("./data.json");
 const fs = require("fs");
+const jwt = require("jsonwebtoken");
 const app = express();
 const { connect } = require("./config/database.js");
 app.use(express.json());
 
-console.log('---- APP START')
+console.log("---- APP START");
 
-app.get("/", (req, res) =>
-  res.json({
-    message: "works",
-  })
-);
+// app.get("/", (req, res) =>
+//   res.json({
+//     message: "works",
+//   })
+// );
+
+app.get("/api/posts", verifyToken, (req, res) => {
+
+  jwt.verify(req.token, "secretkey", (err, authDate) => {
+    if (err) {
+      res.sendStatus(403); //forbidden
+    } else {
+      res.json({
+        message: "Post created..fffff",
+        authDate,
+      });
+    }
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  const user = {
+    id: 1,
+    username: "Jane",
+    email: "jane@gmail.com",
+  };
+
+  jwt.sign({ user: user }, "secretkey", (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  // console.log(bearerHeader);
+  if (typeof bearerHeader !== "undefined") {
+    const bearerToken = bearerHeader.split(" ")[1];
+    req.token = bearerToken;
+    console.log('--- ', req.token)
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 
 app.get("/api/v1/restaurants", (req, res) => {
-  console.log(data.restarants);
   return res.json(data);
 });
 
@@ -35,7 +76,6 @@ app.put("/api/v1/restaurants", (req, res) => {
   const index = data.restaurants.findIndex(
     (aRestaurant) => aRestaurant.id === +body.id
   );
-  console.log(index, data.restaurants.length);
   if (index > -1) {
     data.restaurants.splice(index, 1);
     data.restaurants.push(body);
@@ -66,4 +106,9 @@ app.delete("/api/v1/restaurants/:restaurantId", (req, res) => {
 });
 
 connect();
-app.listen(4000, () => console.log("Running"));
+app.listen(4000, () => console.log("Running on 4000"));
+
+
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWxleCIsImlhdCI6MTYwMDgwMzI1Nn0.d4E9DvucVTfAmpcfCKOL_q3pTmdEiV0Wz74JknI2i3U
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6IkphbmUiLCJlbWFpbCI6ImphbmVAZ21haWwuY29tIn0sImlhdCI6MTYwMDk3ODI3NX0.MdHwzS8GBI5pm56bwZUluJZ9Y_9B4pYiOEqd6suRTvQ

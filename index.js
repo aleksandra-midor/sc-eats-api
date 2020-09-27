df771c58131c32bd5aa6b63156dab5d70f9e356f
octane\ola <ola_midor@outlook.com>
2020-09-27 13:26:40 +0200

    JWT authorization added

--- index.js
+++ index.js
@@ -8,107 +8,138 @@ app.use(express.json());
 
 console.log("---- APP START");
 
-// app.get("/", (req, res) =>
-//   res.json({
-//     message: "works",
-//   })
-// );
+const users = [{
+  username: "Mark",
+  email: "mark@gmail.com",
+  password: "january"
+},
+{
+  username: "Tom",
+  email: "tom@gmail.com",
+  password: "december"
+}]
 
-app.get("/api/posts", verifyToken, (req, res) => {
+// login
+app.post("/api/v1/login", (req, res) => {
+ 
+  const { body } = req
+  const { username } = body
+  const { password } = body
 
+  console.log(body)
+
+ const foundUser = users.find(user => {
+   return user.username === username && user.password === password
+ })
+console.log(foundUser)
+ if (foundUser) {
+
+   jwt.sign({ user: foundUser }, "secretkey", 
+  //  {expiresIn: "1h"}, 
+   (err, token) => {
+     res.json({
+       token,
+      });
+    });
+  } else {
+    res.send( { message: 'Username or password are incorrect.'})
+  }
+  });
+
+  function verifyToken(req, res, next) {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== "undefined") {
+      const bearerToken = bearerHeader.split(" ")[1];
+      req.token = bearerToken;
+      console.log("--- ", req.token);
+      next();
+    } else {
+      res.sendStatus(403);
+    }
+  }
+  
+// get
+app.get("/api/v1/restaurants", verifyToken, (req, res) => {
   jwt.verify(req.token, "secretkey", (err, authDate) => {
     if (err) {
       res.sendStatus(403); //forbidden
     } else {
-      res.json({
-        message: "Post created..fffff",
-        authDate,
-      });
+      return res.json(data);
     }
   });
 });
 
-app.post("/api/login", (req, res) => {
-  const user = {
-    id: 1,
-    username: "Jane",
-    email: "jane@gmail.com",
-  };
+// add
+app.post("/api/v1/restaurants", verifyToken, (req, res) => {
+  // console.log(req.body); // body --> sending body
+  jwt.verify(req.token, "secretkey", (err, authDate) => {
+    if (err) {
+      res.sendStatus(403); //forbidden
+    } else {
+      const body = req.body;
+      console.log(body)
+      body.id = data.restaurants.length + 1;
+      data.restaurants.push(body);
 
-  jwt.sign({ user: user }, "secretkey", (err, token) => {
-    res.json({
-      token,
-    });
+      fs.writeFileSync("./data-test.json", JSON.stringify(data, null, 2));
+      return res.json(data);
+    }
   });
 });
 
-function verifyToken(req, res, next) {
-  const bearerHeader = req.headers["authorization"];
-  // console.log(bearerHeader);
-  if (typeof bearerHeader !== "undefined") {
-    const bearerToken = bearerHeader.split(" ")[1];
-    req.token = bearerToken;
-    console.log('--- ', req.token)
-    next();
-  } else {
-    res.sendStatus(403);
-  }
-}
-
-app.get("/api/v1/restaurants", (req, res) => {
-  return res.json(data);
-});
-
-// get, delete, post, put
-app.post("/api/v1/restaurants", (req, res) => {
+// change
+app.put("/api/v1/restaurants", verifyToken, (req, res) => {
   // console.log(req.body); // body --> sending body
-  const body = req.body;
-  body.id = data.restaurants.length + 1;
-  data.restaurants.push(body);
-  fs.writeFileSync("./data-test.json", JSON.stringify(data, null, 2));
-  return res.json(data);
+  jwt.verify(req.token, "secretkey", (err, authDate) => {
+    if (err) {
+      res.sendStatus(403); //forbidden
+    } else {
+      const body = req.body;
+      const index = data.restaurants.findIndex(
+        (aRestaurant) => aRestaurant.id === +body.id
+      );
+      if (index > -1) {
+        data.restaurants.splice(index, 1);
+        data.restaurants.push(body);
+      }
+
+      fs.writeFileSync("./data-test.json", JSON.stringify(data, null, 2));
+      return res.json(data);
+    }
+  });
 });
 
-app.put("/api/v1/restaurants", (req, res) => {
-  // console.log(req.body); // body --> sending body
-  const body = req.body;
-
-  const index = data.restaurants.findIndex(
-    (aRestaurant) => aRestaurant.id === +body.id
-  );
-  if (index > -1) {
-    data.restaurants.splice(index, 1);
-    data.restaurants.push(body);
-  }
-
-  fs.writeFileSync("./data-test.json", JSON.stringify(data, null, 2));
-  return res.json(data);
+// getting element by a parameter
+app.get("/api/v1/restaurants/:restaurantId", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authDate) => {
+    if (err) {
+      res.sendStatus(403); //forbidden
+    } else {
+      const restaurantId = req.params.restaurantId;
+      const restaurant = data.restaurants.find(
+        (aRestaurant) => aRestaurant.id === +restaurantId
+      );
+      return res.json(restaurant);
+    }
+  });
 });
 
-app.get("/api/v1/restaurants/:restaurantId", (req, res) => {
-  // getting a parameter
-  const restaurantId = req.params.restaurantId;
-  const restaurant = data.restaurants.find(
-    (aRestaurant) => aRestaurant.id === +restaurantId
-  );
-  return res.json(restaurant);
-});
+// deleting element by a parameter
+app.delete("/api/v1/restaurants/:restaurantId", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authDate) => {
+    if (err) {
+      res.sendStatus(403); //forbidden
+    } else {
+      const restaurantId = req.params.restaurantId;
+      const restaurant = data.restaurants.filter(
+        (aRestaurant) => aRestaurant.id !== +restaurantId
+      );
 
-app.delete("/api/v1/restaurants/:restaurantId", (req, res) => {
-  // getting a parameter
-  const restaurantId = req.params.restaurantId;
-  const restaurant = data.restaurants.filter(
-    (aRestaurant) => aRestaurant.id !== +restaurantId
-  );
-
-  fs.writeFileSync("./data-test.json", JSON.stringify(restaurant, null, 2));
-  return res.json(restaurant);
+      fs.writeFileSync("./data-test.json", JSON.stringify(restaurant, null, 2));
+      return res.json(restaurant);
+    }
+  });
 });
 
 connect();
 app.listen(4000, () => console.log("Running on 4000"));
-
-
-
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWxleCIsImlhdCI6MTYwMDgwMzI1Nn0.d4E9DvucVTfAmpcfCKOL_q3pTmdEiV0Wz74JknI2i3U
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6IkphbmUiLCJlbWFpbCI6ImphbmVAZ21haWwuY29tIn0sImlhdCI6MTYwMDk3ODI3NX0.MdHwzS8GBI5pm56bwZUluJZ9Y_9B4pYiOEqd6suRTvQ

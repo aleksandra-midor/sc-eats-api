de7b3e9e7a31c2449873187acf0d5721c1a1505f
octane\ola <ola_midor@outlook.com>
2020-09-27 13:31:16 +0200

    code cleanup

--- index.js
+++ index.js
@@ -6,58 +6,55 @@ const app = express();
 const { connect } = require("./config/database.js");
 app.use(express.json());
 
-console.log("---- APP START");
-
-const users = [{
-  username: "Mark",
-  email: "mark@gmail.com",
-  password: "january"
-},
-{
-  username: "Tom",
-  email: "tom@gmail.com",
-  password: "december"
-}]
+const users = [
+  {
+    username: "Mark",
+    email: "mark@gmail.com",
+    password: "january",
+  },
+  {
+    username: "Tom",
+    email: "tom@gmail.com",
+    password: "december",
+  },
+];
 
 // login
 app.post("/api/v1/login", (req, res) => {
- 
-  const { body } = req
-  const { username } = body
-  const { password } = body
+  const { body } = req;
+  const { username } = body;
+  const { password } = body;
 
-  console.log(body)
-
- const foundUser = users.find(user => {
-   return user.username === username && user.password === password
- })
-console.log(foundUser)
- if (foundUser) {
-
-   jwt.sign({ user: foundUser }, "secretkey", 
-  //  {expiresIn: "1h"}, 
-   (err, token) => {
-     res.json({
-       token,
-      });
-    });
-  } else {
-    res.send( { message: 'Username or password are incorrect.'})
-  }
+  const foundUser = users.find((user) => {
+    return user.username === username && user.password === password;
   });
-
-  function verifyToken(req, res, next) {
-    const bearerHeader = req.headers["authorization"];
-    if (typeof bearerHeader !== "undefined") {
-      const bearerToken = bearerHeader.split(" ")[1];
-      req.token = bearerToken;
-      console.log("--- ", req.token);
-      next();
-    } else {
-      res.sendStatus(403);
-    }
+  if (foundUser) {
+    jwt.sign(
+      { user: foundUser },
+      "secretkey",
+      //  {expiresIn: "1h"},
+      (err, token) => {
+        res.json({
+          token,
+        });
+      }
+    );
+  } else {
+    res.send({ message: "Username or password are incorrect." });
   }
-  
+});
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearerToken = bearerHeader.split(" ")[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
 // get
 app.get("/api/v1/restaurants", verifyToken, (req, res) => {
   jwt.verify(req.token, "secretkey", (err, authDate) => {
@@ -71,13 +68,11 @@ app.get("/api/v1/restaurants", verifyToken, (req, res) => {
 
 // add
 app.post("/api/v1/restaurants", verifyToken, (req, res) => {
-  // console.log(req.body); // body --> sending body
   jwt.verify(req.token, "secretkey", (err, authDate) => {
     if (err) {
       res.sendStatus(403); //forbidden
     } else {
       const body = req.body;
-      console.log(body)
       body.id = data.restaurants.length + 1;
       data.restaurants.push(body);
 
@@ -89,7 +84,6 @@ app.post("/api/v1/restaurants", verifyToken, (req, res) => {
 
 // change
 app.put("/api/v1/restaurants", verifyToken, (req, res) => {
-  // console.log(req.body); // body --> sending body
   jwt.verify(req.token, "secretkey", (err, authDate) => {
     if (err) {
       res.sendStatus(403); //forbidden
